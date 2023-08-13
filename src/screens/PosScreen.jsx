8b654cc0a1848beaf1e78c2032e1f7493f3a2313
Alain Perez Balart <alain.perez@openbravo.com>
2023-08-13 17:20:10 -0400

    customizing header

--- src/screens/PosScreen.jsx
+++ src/screens/PosScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 import {Block, Text, Button, theme} from 'galio-framework';
 
 const headerLeft = ({navigation}) => {
@@ -14,18 +15,47 @@ const headerLeft = ({navigation}) => {
     );
   };
 };
+const HeaderQtyProducts = () => {
+  return (
+    <Block width={35} height={33} style={headerQtyProductsStyles.container}>
+      <Block flex={0.9} style={headerQtyProductsStyles.inner}>
+        <Text color={'white'}>1</Text>
+      </Block>
+    </Block>
+  );
+};
+const headerQtyProductsStyles = StyleSheet.create({
+  inner: {
+    backgroundColor: theme.COLORS.PRIMARY,
+    width: '90%',
+    marginLeft: 2,
+    marginTop: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    marginLeft: 10,
+    backgroundColor: 'white',
+  },
+});
 const headerTitle = props => {
   return () => {
     return (
-      <Block flex row>
-        <Text>POS</Text>
-      </Block>
+      <TouchableOpacity>
+        <Block flex row style={headerTitleStyles.container}>
+          <Text color={'white'}>VENTA</Text>
+          <HeaderQtyProducts />
+        </Block>
+      </TouchableOpacity>
     );
   };
 };
+const headerTitleStyles = StyleSheet.create({
+  container: {alignItems: 'center'},
+});
 const headerRight = props => {
   return () => {
-    return <FontAwesome6 name="star" size={23} color="#fff" />;
+    return <Fontisto name="spinner-refresh" size={23} color="#fff" />;
   };
 };
 const PosScreen = ({navigation}) => {
