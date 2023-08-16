8017849f9d21d667baa6421cd61d8b4dce7f7976
Alain Perez Balart <alain.perez@openbravo.com>
2023-08-15 23:14:34 -0400

    Added custom drawer

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 import MainStackNavigator from './StackNavigator';
-import {theme} from 'galio-framework';
+import {theme, Block} from 'galio-framework';
 import DetailsScreen from '../screens/DetailsScreen';
-import Pos from '../screens/PosScreen';
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props} contentContainerStyle={{paddingTop: 0}}>
+      <Block
+        style={{backgroundColor: theme.COLORS.PRIMARY, height: 100, margin: 0}}
+      />
+      <DrawerItemList {...props} />
+      <DrawerItem label="Help" onPress={() => alert('Link to help')} />
+    </DrawerContentScrollView>
+  );
+}
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = ({navigation}) => {
   return (
     <Drawer.Navigator
+      drawerContent={props => <CustomDrawerContent {...props} />}
       screenOptions={{
         headerStyle: {
           //backgroundColor: '#f4511e',
